4620a35d166d88a407762346bc0ab8bd4a361cf9
Thomas McKee <caffeine.storm@gmail.com>
2026-04-17 23:47:14 -0400

    more block tests

--- txt2dot/test/Parse/BlockSpec.hs
+++ txt2dot/test/Parse/BlockSpec.hs
@@ -14,6 +14,11 @@ blockExample = unlines [
   "\tlvl2 too"
   ]
 
+oneLineBlock :: String
+oneLineBlock = "- technically, a block"
+oneLineBlockLabel :: String
+oneLineBlockLabel = drop 2 oneLineBlock
+
 blockAlone :: String
 blockAlone = unlines [
   "- start block",
@@ -33,3 +38,8 @@ spec = do
         ])
     it "can handle a single block as the whole input" $ do
       (parseText blockAlone) `shouldBe` (Just $ leafNode blockAloneLabel)
+    it "can hanlde a sinlge-line block" $ do
+      (parseText oneLineBlock) `shouldBe` (Just $ leafNode oneLineBlockLabel)
+    it "can handle two blocks, one after the other" $ do
+      (parseText (unlines [oneLineBlock, oneLineBlock])) `shouldBe`
+        (Just $ TodoNode "" [leafNode oneLineBlockLabel, leafNode oneLineBlockLabel])
